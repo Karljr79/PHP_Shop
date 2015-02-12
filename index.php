1ced90df675a0c845831aa7d67e75f10a66b759a
Karl Hirschhorn <karljr79@gmail.com>
2015-02-12 22:20:34 +0000

    Cleanup

--- index.php
+++ index.php
@@ -33,6 +33,7 @@ include('inc/header.php'); ?>
 						$list_view_html = "";
 						foreach($products as $product_id => $product) {
 							$position = $position + 1;
+
 							if ($total_products - $position < 4)
 							$list_view_html = get_list_view_html($product_id, $product) . $list_view_html;
 						}
